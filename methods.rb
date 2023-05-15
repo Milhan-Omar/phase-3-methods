81a047ee4c2d762f61a44257ed2bd797bd8cbbe7
Milhan Osman <milhanomarosman@gmail.com>
2023-05-15 17:33:37 +0300

    completed assigment

--- methods.rb
+++ methods.rb
@@ -1,27 +1,35 @@
 # Your code here!
+
 def greet_programmer()
  puts "Hello, programmer!"
 end
 
-def greet(name = "Naureen")
+def greet(name)
   puts "Hello, #{name}!"
 end
+greet("Naureen")
+
 
 def greet_with_default(name = "programmer")
   puts "Hello, #{name}!"
 end
-
-def add(num1, num2)
-  num1 + num2
-end
+greet_with_default()
 
 
-def halve(number)
-    if number % 2 == 0
-        return number / 2
-    else
-        return nil
-    end
-end
+
+ def add (num1 , num2)
+ return num1 + num2
+  end
+  puts add(2,5)
+
+
+  def halve (num)
+   if num % 2 == 0
+   return num / 2
+  else
+   return nil
+  end
+  end
+  puts halve(2)
 
 
